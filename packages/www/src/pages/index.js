a8daaa38801d659ede8b149f2ef9460a8d7a50de
Jull Weber <mkykode@gmail.com>
2020-02-19 16:32:31 -0800

    add identity widghet

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, Heading, Button, Flex } from 'theme-ui'
+import netlifyIdentity from 'netlify-identity-widget'
+
 export default props => {
+  useEffect(() => {
+    netlifyIdentity.init({})
+  }, [])
   return (
     <Container>
       <Flex sx={{ flexDirection: 'column', padding: 3 }}>
@@ -8,10 +13,19 @@ export default props => {
         <Button
           sx={{ marginTop: 2 }}
           onClick={() => {
-            alert('clicked')
+            netlifyIdentity.open()
           }}
         >
           Log in
+        </Button>
+        <Button
+          sx={{ marginTop: 2 }}
+          onClick={() => {
+            const currentUser = netlifyIdentity.currentUser()
+            console.log(currentUser)
+          }}
+        >
+          Current User
         </Button>
       </Flex>
     </Container>
